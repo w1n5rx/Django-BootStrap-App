119bc3ee5db6f5bfd83c216f4506f9b04dc15d08
w1n5rx <vinod.ramalwan@stud.tu-darmstadt.de>
2017-08-12 20:22:35 +0200

    Stylesheet changes

--- app/views.py
+++ app/views.py
@@ -3,31 +3,35 @@ from app.models import Sites
 from django.db.models import Sum
 from django.db import connection
 
+CONST_DEMO = "Demo Site"
+CONST_ABC = "ABC Site"
+CONST_XYZ = "XYZ Site"
+
 # Create your views here.
 Sites.objects.all().delete()
 
 # if Sites is None:
 data = Sites.objects.bulk_create([
-    Sites(date_value='2015-02-01', A_value=12.00, B_value=16.00, site_name='Demo Site'),
-    Sites(date_value='2015-02-03', A_value=20.00, B_value=100.00, site_name='Demo Site'),
-    Sites(date_value='2015-02-10', A_value=20.00, B_value=80.00, site_name='Demo Site'),
-    Sites(date_value='2015-02-03', A_value=5.00, B_value=15.00, site_name='ABC Site'),
-    Sites(date_value='2015-02-15', A_value=5.00, B_value=15.00, site_name='XYZ Site'),
-    Sites(date_value='2015-02-28', A_value=5.00, B_value=15.00, site_name='XYZ Site'),
+    Sites(date_value='2015-02-01', A_value=12.00, B_value=16.00, site_name=CONST_DEMO),
+    Sites(date_value='2015-02-03', A_value=20.00, B_value=100.00, site_name=CONST_DEMO),
+    Sites(date_value='2015-02-10', A_value=20.00, B_value=80.00, site_name=CONST_DEMO),
+    Sites(date_value='2015-02-03', A_value=5.00, B_value=15.00, site_name=CONST_ABC),
+    Sites(date_value='2015-02-15', A_value=5.00, B_value=15.00, site_name=CONST_XYZ),
+    Sites(date_value='2015-02-28', A_value=5.00, B_value=15.00, site_name=CONST_XYZ),
 ])
 def home(request):
     return render(request, 'app/index.html')
 
 def summary(request):
-    args = {'sites': [{'site_name':'Demo Site', 
-                        'A_value': (Sites.objects.filter(site_name='Demo Site').aggregate(Sum('A_value')))['A_value__sum'], 
-                        'B_value': (Sites.objects.filter(site_name='Demo Site').aggregate(Sum('B_value')))['B_value__sum']},
-                    {'site_name':'ABC Site',                         
-                        'A_value': (Sites.objects.filter(site_name='ABC Site').aggregate(Sum('A_value')))['A_value__sum'], 
-                        'B_value': (Sites.objects.filter(site_name='ABC Site').aggregate(Sum('B_value')))['B_value__sum']},
-                    {'site_name':'XYZ Site',                         
-                        'A_value': (Sites.objects.filter(site_name='XYZ Site').aggregate(Sum('A_value')))['A_value__sum'], 
-                        'B_value': (Sites.objects.filter(site_name='XYZ Site').aggregate(Sum('B_value')))['B_value__sum']}]}
+    args = {'sites': [{'site_name':CONST_DEMO, 
+                        'A_value': (Sites.objects.filter(site_name=CONST_DEMO).aggregate(Sum('A_value')))['A_value__sum'], 
+                        'B_value': (Sites.objects.filter(site_name=CONST_DEMO).aggregate(Sum('B_value')))['B_value__sum']},
+                    {'site_name':CONST_ABC,                         
+                        'A_value': (Sites.objects.filter(site_name=CONST_ABC).aggregate(Sum('A_value')))['A_value__sum'], 
+                        'B_value': (Sites.objects.filter(site_name=CONST_ABC).aggregate(Sum('B_value')))['B_value__sum']},
+                    {'site_name':CONST_XYZ,                         
+                        'A_value': (Sites.objects.filter(site_name=CONST_XYZ).aggregate(Sum('A_value')))['A_value__sum'], 
+                        'B_value': (Sites.objects.filter(site_name=CONST_XYZ).aggregate(Sum('B_value')))['B_value__sum']}]}
     return render(request, 'app/summary.html', args)
 
 def summaryavg(request):
@@ -40,7 +44,7 @@ def summaryavg(request):
         b_val_demo = cursor.fetchone()
 
         # Calculate average for ABC Site
-        cursor.execute('SELECT AVG(A_value) FROM app_sites where site_name="ABC Site"')
+        cursor.execute('SELECT ROUND(AVG(A_value), 2) FROM app_sites where site_name="ABC Site"')
         a_val_abc = cursor.fetchone()
         cursor.execute('SELECT ROUND(AVG(B_value), 2) FROM app_sites where site_name="ABC Site"')
         b_val_abc = cursor.fetchone()
@@ -51,13 +55,13 @@ def summaryavg(request):
         cursor.execute('SELECT ROUND(AVG(B_value), 2) FROM app_sites where site_name="XYZ Site"')
         b_val_xyz = cursor.fetchone()
 
-        args = {'sites': [{'site_name':'Demo Site', 
+        args = {'sites': [{'site_name':CONST_DEMO, 
                             'A_value': a_val_demo[0], 
                             'B_value': b_val_demo[0]},
-                        {'site_name':'ABC Site', 
+                        {'site_name':CONST_ABC, 
                             'A_value': a_val_abc[0], 
                             'B_value': b_val_abc[0]},
-                        {'site_name':'XYZ Site', 
+                        {'site_name':CONST_XYZ, 
                             'A_value': a_val_xyz[0], 
                             'B_value': a_val_xyz[0]}
                         ]
@@ -76,12 +80,12 @@ def sites(request, site_id):
     path = request.path_info.lstrip('/')
     print('path', path)
 
-    args = {'sites': Sites.objects.filter(site_name='Demo Site'), 'site_name': Sites.objects.filter(site_name='Demo Site')[0].site_name}
+    args = {'sites': Sites.objects.filter(site_name=CONST_DEMO), 'site_name': Sites.objects.filter(site_name=CONST_DEMO)[0].site_name}
     if path == 'sites/1/':
-        args = {'sites': Sites.objects.filter(site_name='Demo Site'), 'site_name': Sites.objects.filter(site_name='Demo Site')[0].site_name}
+        args = {'sites': Sites.objects.filter(site_name=CONST_DEMO), 'site_name': Sites.objects.filter(site_name=CONST_DEMO)[0].site_name}
     elif path == 'sites/2/':
-        args = {'sites': Sites.objects.filter(site_name='ABC Site'), 'site_name': Sites.objects.filter(site_name='ABC Site')[0].site_name}
+        args = {'sites': Sites.objects.filter(site_name=CONST_ABC), 'site_name': Sites.objects.filter(site_name=CONST_ABC)[0].site_name}
     elif path == 'sites/3/':
-        args = {'sites': Sites.objects.filter(site_name='XYZ Site'), 'site_name': Sites.objects.filter(site_name='XYZ Site')[0].site_name}
+        args = {'sites': Sites.objects.filter(site_name=CONST_XYZ), 'site_name': Sites.objects.filter(site_name=CONST_XYZ)[0].site_name}
 
     return render(request, 'app/sites/' +site_id+ '.html', args)
